7c4adeeca9ddfbefab719a718b20326d370058f9
Lemonice3003 <142006779+LemoNice-3003@users.noreply.github.com>
2025-06-03 00:41:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function saveDetaUpload (e) {
     (async () => {
         const files = await showOpenFileDialog();   //fileにダイアログで選択したtxtファイルのデータを渡す
         const content = await files[0].text();  //contentに開いたtxtファイルの中身を移す
-        // alert(content);
         document.getElementById("output").innerHTML = content;  //読み込んだ内容の表示
         if (content == "0") {
             $(function changeColor(){
@@ -27,16 +26,6 @@ function saveDetaUpload (e) {
         }
     })();
 }
-// ②IMG要素をすべてセレクト
-document.querySelectorAll("img").forEach((imgElm) => {
-    // ③要素のクリックイベントにイベントリスナーをひもづける
-    imgElm.addEventListener('click', saveDetaUpload);
-})
-document.addEventListener('keydown', function(event) {
-    if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key === '-' || event.key === '0')) {
-      event.preventDefault();
-    }
-});
 
 document.addEventListener('wheel', function(event) {
     if (event.ctrlKey) {
@@ -48,11 +37,9 @@ const con = document.querySelector('#js-con');
 const ham = document.querySelector('#js-hamburger');
 const nav = document.querySelector('#js-nav');
 ham.addEventListener('click', function () {
-
   con.classList.toggle('active');
   ham.classList.toggle('active');
   nav.classList.toggle('active');
-
 });
 // $(function(){
 //    $("span").css({"color" : "red", "font-size" : "100px", "border" : "solid 5px"});
